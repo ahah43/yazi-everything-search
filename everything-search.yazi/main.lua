bf1cc1394f5869f9e41617ee01bdf47c0dcb5b5f
Ahmed Hassan <ahah432@yahoo.com>
2025-07-25 16:50:07 +0400

    v0.8 testssss

--- everything-search.yazi/main.lua
+++ everything-search.yazi/main.lua
@@ -1,9 +1,9 @@
-return {
-    entry = function()
+-- return {
+    -- entry = function()
             ya.notify({
                 title = "es plugin",
                 content = "Not in a git directory\nError: ",
                 level = "warn",
                 timeout = 5,
             })
-}
+-- }
