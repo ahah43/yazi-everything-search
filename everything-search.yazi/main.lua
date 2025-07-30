69a83826934a56cb6bb0ebb2032105e1a8bc1a43
Ahmed Hassan <ahah432@yahoo.com>
2025-07-30 09:48:37 +0400

    hope

--- everything-search.yazi/main.lua
+++ everything-search.yazi/main.lua
@@ -80,8 +80,8 @@ local function entry(_)
     local output, err = child:wait_with_output()
     if not output then
         return fail("Cannot read command output, error code %s", err)
-    elseif not output.status.success and output.status.code ~= 130 then
-        return fail("Spawn command exited with error code %s", output.status.code)
+    -- elseif not output.status.success and output.status.code ~= 130 then
+    --     return fail("Spawn command exited with error code %s", output.status.code)
     end
 
     local target = output.stdout:gsub("\n$", "")
