73ef10adad86a01fd2915b0ef0646c463cb86fcc
Ahmed Hassan <ahah432@yahoo.com>
2025-07-30 09:33:59 +0400

    v0.00

--- everything-search.yazi/main.lua
+++ everything-search.yazi/main.lua
@@ -56,13 +56,20 @@ local function entry(_)
         return -- Exit the plugin
     end
 
+    ya.notify({
+        title = "Search Started v123",
+        content = "search_command = " .. es_search_command,
+        level = "info",
+        timeout = 5
+    })
+
     -- local h = hovered()
     -- local parentDir = h.url.base
     local parentDir = root()
     local es_search_command = string.format('es "%s" -path "%s" | fzf', query, parentDir)
 
-    local child, err = Command("cmd"):arg({"/c", cmd_args}):stdin(Command.INHERIT):stdout(Command.PIPED)
-        :stderr(Command.PIPED):spawn()
+    local child, err = Command("cmd"):arg({"/c", cmd_args}):stdin(Command.INHERIT):stdout(Command.PIPED):stderr(
+        Command.PIPED):spawn()
 
     if not child then
         return fail("Spawn command failed with error code %s.", err)
