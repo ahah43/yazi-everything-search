aa370037e38bcd29253b36a81329afe59c89bcf1
Ahmed Hassan <ahah432@yahoo.com>
2025-07-30 09:26:50 +0400

    added input prompt

--- everything-search.yazi/main.lua
+++ everything-search.yazi/main.lua
@@ -1,13 +1,66 @@
 local function fail(s, ...)
 	ya.notify { title = "my_plugin_name", content = string.format(s, ...), timeout = 5, level = "error" }
 end
+--- @since 25.5.31
+
+local root = ya.sync(function()
+    return cx.active.current.cwd
+end
+)
+
+
+
+local hovered = ya.sync(function()
+    local h = cx.active.current.hovered
+    if not h then
+        return {}
+    end
+    return {
+        url = h.url,
+        is_dir = h.cha.is_dir,
+        unique = #cx.active.current.files == 1
+    }
+    end
+)
+
+
+local function prompt()
+    return ya.input {
+        title = "EveryThing Search:",
+        pos = {"center",w = 50},
+        position = {"center",w = 50}, -- TODO: remove
+        -- realtime = false,debounce = 0.1
+    }
+end
 
 local function entry(_)
 	local _permit = ya.hide()
 	local cmd_args = "es folder: | fzf"
 
-	local child, err = Command("cmd")
-		:arg({ "/c", cmd_args })
+    local query, event = prompt()
+
+-- Check if the user cancelled or provided an empty query.
+    if not query or query:len() == 0 then
+        ya.notify({
+            title = "Search Cancelled v123",
+            content = "What to search for?",
+            level = "info",
+            timeout = 5
+            })
+        return -- Exit the plugin
+    end
+
+    -- local h = hovered()
+    -- local parentDir = h.url.base
+    local parentDir = root()
+    local es_search_command = string.format('es "%s" -path "%s"',
+    query, parentDir)
+
+
+
+
+local child, err = Command("cmd")
+		:arg({ "/c", es_search_command })
 		:stdin(Command.INHERIT)
 		:stdout(Command.PIPED)
 		:stderr(Command.PIPED)
